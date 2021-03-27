da8520671e8142e3ae7e2e5dbdbf663094ee799f
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-27 21:57:24 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,40 @@
+const keys = document.querySelectorAll(".key");
+const textArea = document.querySelector("#textarea");
+let output = [];
+const specialKeys = ["capsLock", "space", "backSpace"];
+let capsLock = false;
 
+
+const upperCase = char => char.toUpperCase();
+
+keys.forEach(key => {
+    key.addEventListener("click", e => {
+        if(specialKeys.indexOf(e.target.value) != -1){
+            if(e.target.value == "capsLock" && capsLock == false){
+                capsLock = true;
+            }else if(capsLock == true){
+                capsLock = false;
+            }else if(e.target.value === "space"){
+                output.push(" ");
+                textArea.innerText += output.join("");
+            }else if(e.target.value = "backSpace"){
+                output.pop();
+                textArea.innerText += output.join("");
+            }
+            
+        }
+
+        if(capsLock === true && e.target.value != "capsLock") {
+            output.push(e.target.value.toUpperCase());
+            textArea.innerText += output.join("");
+        }
+
+
+        if(specialKeys.indexOf(e.target.value) == -1 && capsLock == false){
+        output.push(e.target.value);
+        textArea.innerText += output.join("");
+        }
+        
+       
+    })
+});
